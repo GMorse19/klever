b226ac561bb53fc12e8dba4075a7ae2bcb97fb05
GMorse19 <malorse@comcast.net>
2021-01-24 22:14:48 -0500

    Removed some pages until further notice

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 
 import Footer from '../../components/Footer/Footer'
-import { stock } from '../../helpers/images/stock/stockArray'
+// import { stock } from '../../helpers/images/stock/stockArray'
 
 import './About.scss'
 
@@ -16,62 +16,7 @@ const About = () => (
                 </div>
             </Row>
             <div className='about-content'>
-            <Row className='justify-content-center'>
-            
-                <Col>
-                <h3>Where We Came From</h3>
-                    <p>
-                    In 1984, Klever Lang celebrated a Golden Gloves title win.
-                    Following that, he began to focus on training and developing
-                    fighters, producing numerous Golden Gloves champions. But
-                    boxing in Jamaica Plain had seen better days, and there was
-                    a pressing need for an authentic gym. 
-                    </p>
-                </Col>
-
-            </Row>
-            <Row className='justify-content-center'>
-            <Col lg={4}>
-                    <p>
-                    A space on South Street opened up, and with its concrete
-                    walls, iron piping, and low ceilings, it had the look and
-                    feel he wanted: a hard space for hard workouts. Sold.
-                    </p>
-            </Col>
-            <Col lg={8}><img src={stock.boxing.boxer} alt='boxer' /></Col>
-            <Col>
-                    <p>
-                    Klever Boxing Gym opened its doors in September of 2010. 
-                    </p>
-                    <p>
-                    Today, Klever is home to aspiring amateurs and local pros
-                    alike. With a friendly atmosphere and classes designed to teach
-                    boxing to the masses, we've become home to a growing number of
-                    people who recognize the unparalleled benefit of the boxer's
-                    workout. School teachers, mothers, stockbrokers, dentists,
-                    artists, and lawyers all come to experience and reap the benefits
-                    of our expertly guided classes and training.
-                    </p>
-                    <p>
-                    And we're just getting started. Come be a part of it!
-                    </p>
-                    <Button>TAKE A FREE BEGINNERS CLASS</Button>
-                </Col>
-                
-            </Row>
-            <hr />
-            <Row>
-            <Col>
-            <p><img src={stock.boxing.clubber} alt='clubber lang'/></p>
-            </Col>
-            <Col>
-                <div>
-                    <p>"I pity the fool, and I will destroy any man who tries
-                    to take what I got!"</p>
-                    <p>-- Klever Lang</p>
-                </div>
-            </Col>
-            </Row>
+              <h1>Under Construction</h1>
             </div>
             <Row><Footer /></Row>
         </Container>
