05671617b12abb7ffff25f42d50deec607cd2f28
GMorse19 <malorse@comcast.net>
2021-03-23 21:49:29 -0400

    Added an intro section to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,8 +13,17 @@ const Home = () => (
         <div><p className='home-page-title'>Klever Boxing</p></div>
       </Col>
     </Row>
+    <Row className='justify-content-center'>
+      <Col lg={6} xs={6} className='home-page-intro'>
+        <p>
+          This is where we will put a brief description of what Klever Boxing is all about.
+        </p>
+      </Col>
+    </Row>
   </Container>
-  <Footer />
+  <div className='home-footer'>
+    <Footer />
+  </div>
   </div>
 )
 
